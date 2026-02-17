64e22e71be30775aebabac8c14c2770dba81456b
RT-Jeion <rejuwan002@gmail.com>
2026-02-18 01:49:02 +0600

    Feb 18, 01:48

--- Contact Book CLI Project/main.py
+++ Contact Book CLI Project/main.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 class Contact:
     def __init__(self, name, phone, email, adress):
         self.name = name
@@ -5,4 +8,48 @@ class Contact:
         self.email = email
         self.adress = adress
 
-    def to_dict
+    def to_dict():
+        return {
+            "Name": self.name,
+            "Phone": self.phone,
+            "Email": self.email,
+            "Adress": self.adress
+        }
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            name = data["name"],
+            phone = data["phone"],
+            email = data["email"],
+            adress = data["adress"]
+        )
+
+
+Class ContactBook:
+    def __init__(self, name):
+        self.bookName = f"{name}.json"
+
+    def add_contact(self,name, phone, email, adress):
+        contact = Contact(name, phone, email, adress)
+        
+        if os.path.exists(self.bookName):
+            with open(self.bookName) as f:
+                contacts = json.load(f)
+            else:
+                contacts = []
+
+        contacts.append(contact.to_dict())
+        return contacts
+
+    def search_contact(self, keyWord):
+        if not os.path.exists(self.bookName):
+            print("There is not ContactBook")
+            return
+
+        with open(self.bookName, 'r') as f:
+            contacts = json.loaf(f)
+
+        for data in contacts:
+            if keyWord in data.keys():
+                return data
+        return "Contact Nof Found"
