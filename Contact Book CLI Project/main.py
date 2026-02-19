dce8daa762b41810cd5e9ddcbce3e2f3b0015433
RT-Jeion <rejuwan002@gmail.com>
2026-02-19 17:25:52 +0600

    Feb 19 5:24 pm

--- Contact Book CLI Project/main.py
+++ Contact Book CLI Project/main.py
@@ -8,7 +8,7 @@ class Contact:
         self.email = email
         self.adress = adress
 
-    def to_dict():
+    def to_dict(self):
         return {
             "Name": self.name,
             "Phone": self.phone,
@@ -17,6 +17,7 @@ class Contact:
         }
     @classmethod
     def from_dict(cls, data):
+        print("New contact object Created")
         return cls(
             name = data["name"],
             phone = data["phone"],
@@ -25,20 +26,21 @@ class Contact:
         )
 
 
-Class ContactBook:
+class ContactBook:
     def __init__(self, name):
         self.bookName = f"{name}.json"
 
     def add_contact(self,name, phone, email, adress):
         contact = Contact(name, phone, email, adress)
-        
+        print(f"New Contact Created:\nName: {name}\nPhone: {phone}\nEmail: {email}\nAddress: {adress}")
         if os.path.exists(self.bookName):
             with open(self.bookName) as f:
                 contacts = json.load(f)
-            else:
+        else:
                 contacts = []
 
         contacts.append(contact.to_dict())
+        print(f"Contact adeed to {self.bookName}")
         return contacts
 
     def search_contact(self, keyWord):
@@ -51,5 +53,19 @@ Class ContactBook:
 
         for data in contacts:
             if keyWord in data.keys():
+                print("Contact Founded.")
                 return data
         return "Contact Nof Found"
+
+if __name__ == "__main__":
+    contact1 = Contact("RT Jeion", "019000000", "rt.jeion@gmail.com", "Gazipur, Dhaka")
+    con = {
+        "name": "Rejuwan",
+        "phone": "0190000",
+        "email": "rt.jeion@gmail.com",
+        "adress": "Gazipur"
+    }
+    contact2 = Contact.from_dict(con)
+print(contact1.to_dict())
+
+print(contact2.to_dict())
